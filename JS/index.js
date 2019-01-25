65f1f2f7d16a8f769e8da219809a0b9ebc8d4f8b
Артем <FlapJack@MacBook-Pro-Artyom.local>
2019-01-26 00:37:13 +0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+$(function() {
+  $(".img-w").each(function() {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url('+imgSrc+')');
+  })
+            
+  
+  $(".img-c").click(function() {
+    let w = $(this).outerWidth()
+    let h = $(this).outerHeight()
+    let x = $(this).offset().left
+    let y = $(this).offset().top
+    
+    
+    $(".active").not($(this)).remove()
+    let copy = $(this).clone();
+    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+    $(".active").css('top', y - 6);
+    $(".active").css('left', x - 6);
+    
+      setTimeout(function() {
+    copy.addClass("positioned")
+  }, 0)
+    
+  }) 
+  
+})
+
+$(document).on("click", ".img-c.active", function() {
+  let copy = $(this)
+  copy.removeClass("positioned active").addClass("postactive")
+  setTimeout(function() {
+    copy.remove();
+  }, 400)
+})
+
+$('.price span:first-child').after("<span class=\"dots\"> </span>");
